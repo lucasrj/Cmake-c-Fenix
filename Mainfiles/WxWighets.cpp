6efe59899a3be2c1147311febfa914f37532d48b
lucasrj <lucasjeppesen@gmail.com>
2020-09-19 14:18:54 +0200

    added projectbuild for normal and wxWightes porjects

---
+++ Mainfiles/WxWighets.cpp
@@ -0,0 +1,9 @@
+#include "main.h"
+
+IMPLEMENT_APP(MyApp);
+
+
+bool MyApp::OnInit()
+{
+
+};
---
+++ Mainfiles/wxWighets.h
@@ -0,0 +1,12 @@
+#include <wx/wx.h>
+
+class MyApp : public wxApp
+{
+private:
+    
+public:
+    virtual bool OnInit();
+};
+
+
+
